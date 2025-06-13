d2362638694ec931129e5db2103028a8e48059bd
Ashley <panelo.ashley@gmail.com>
2025-06-13 23:26:05 +0800

    Added user parameters

--- main.py
+++ main.py
@@ -2,4 +2,11 @@ import requests
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
+user_params = {
+    "token": "7a6sf5sdfs6df76dys78",
+    "username": "Ashley",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+
+}
 requests.post()
