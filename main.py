9a15aa9af8dc5f6de380cccee89fc9a8256813b0
Ashley <panelo.ashley@gmail.com>
2025-06-14 14:20:51 +0800

    Initialized a request that adds a new graph using the token as header

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 import requests
 
+USERNAME = "ashl3y"
+TOKEN = "7a6sf5sdfs6df76dys78"
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
-    "token": "7a6sf5sdfs6df76dys78",
-    "username": "ashley",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 
 }
-response = requests.post(url=pixela_endpoint, json=user_params)
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Cycling Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "ajisai"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 print(response.text)
